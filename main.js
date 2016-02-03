81a40a54be304617b438cc6a65e88cf218c7438b
Robert Schuh <github@eneticum.de>
2016-02-03 18:26:03 +0100

    basic logging

---
+++ main.js
@@ -0,0 +1,47 @@
+
+	"use strict";
+
+	Widget.initialize({
+		title: "Basic Log Viewer",
+		width: 300,
+		height: 400,
+	}, widget => {
+
+		// debugging
+		console.log("widget", widget);
+		
+		var tabs = eowTabs("div", {}, [{
+			name: "Log View",
+			content: []
+		}]);
+		
+		widget.appendChild(tabs);
+		
+		tabs.selectTab("Log View");
+
+		var net = require('net');
+		var HOST = '127.0.0.1';
+		var PORT = 3273;
+
+		var datatab = tabs.getTab("Log View");
+
+		net.createServer(function(sock) {
+
+			// We have a connection - a socket object is assigned to the connection automatically
+			console.log('CONNECTED: ' + sock.remoteAddress +':'+ sock.remotePort);
+
+			// Add a 'data' event handler to this instance of socket
+			sock.on('data', function(data) {
+
+				eowEl(datatab).appendChildren(data.toString("utf8").split("\n").map(row => eowEl("div", { innerHTML: row })));
+				datatab.scrollTop = datatab.scrollHeight;
+
+			});
+
+			// Add a 'close' event handler to this instance of socket
+			sock.on('close', function(data) {
+				console.log('CLOSED: ' + sock.remoteAddress +' '+ sock.remotePort);
+			});
+
+		}).listen(PORT, HOST);
+	});
